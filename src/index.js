b50c88eae295481e304f0bc6884697659dfdd6aa
Nguyen Thai Hung <19110221@student.hcmute.edu.vn>
2022-09-19 00:37:58 +0700

    install husky

--- src/index.js
+++ src/index.js
@@ -7,26 +7,26 @@ const app = express();
 const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 //HTTP logger
 app.use(morgan('combined'));
 //Template engines
-app.engine('handlebars', handlebars.engine(
-  {
-    extname: '.handlebars',
-  }
-),
+app.engine(
+    'handlebars',
+    handlebars.engine({
+        extname: '.handlebars',
+    }),
 );
 app.set('view engine', 'handlebars');
-           app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //routes init
 route(app);
 
 app.listen(5000, () => {
-  console.log('listening on port 5000');
+    console.log('listening on port 5000');
 });
