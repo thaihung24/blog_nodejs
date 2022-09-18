61fb95d5dd49b7e8422d323a82e4ba9bbdcfea6a
Nguyen Thai Hung <19110221@student.hcmute.edu.vn>
2022-09-19 00:03:21 +0700

    install prettier ,husky,lint-staged

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const { ppid } = require('process');
 const app = express();
-
+const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //HTTP logger
 app.use(morgan('combined'));
-
-
-console.log(__dirname)
 //Template engines
-app.engine('handlebars', handlebars.engine({
-    extname: '.handlebars',
-})); 
+app.engine('handlebars',
+         handlebars.engine({
+             extname: '.handlebars',
+    }),
+);
 app.set('view engine', 'handlebars');
-app.set('views',path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
+//routes init
+route(app);
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('listening on port 5000');
-})
+});
